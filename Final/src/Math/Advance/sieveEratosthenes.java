7e2618d4d53d975b5421ef84f419250f71d061ec
GarvGupta-2005 <garv.0603@gmail.com>
2025-05-11 17:49:50 +0530

    Update sieveEratosthenes.java

--- Final/src/Math/Advance/sieveEratosthenes.java
+++ Final/src/Math/Advance/sieveEratosthenes.java
@@ -6,7 +6,7 @@ public class sieveEratosthenes  {
     public static void sieve(int n){
         int [] prime = new int[n+1];
         Arrays.fill(prime, 1);
-        for(int i = 2;i<=n;i++){
+        for(int i = 2;i*i<=n;i++){
             if(prime[i]==1){
                 for(int j = i*i;j<=n;j+=i){
                     prime[j] = 0;
