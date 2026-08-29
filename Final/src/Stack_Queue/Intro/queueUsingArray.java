1ff061c33d703e88a4613ef4f1610ba276ccf3a1
GarvGupta-2005 <garv.0603@gmail.com>
2026-08-29 13:46:32 +0530

    Add array-based queue implementation
    Implemented a queue using an array with enqueue, dequeue, and front/rear access methods.

--- Final/src/Stack_Queue/Intro/queueUsingArray.java
+++ Final/src/Stack_Queue/Intro/queueUsingArray.java
@@ -73,3 +73,69 @@ public class queueUsingArray {
         System.out.println("The size of the queue after deleting an element " + q.size());
     }
 }
+
+
+class myQueue {
+    private int[] arr;
+    private int capacity;
+    private int size;
+
+    // Constructor
+    public myQueue(int capacity) {
+         //Maximum number of elements the queue can hold.
+        this.capacity = capacity;
+         // Array to store queue elements.
+        arr = new int[capacity];
+        // Current number of elements in the queue.
+        size = 0;
+    }
+
+    // Check if queue is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Check if queue is full
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    // Enqueue
+    public void enqueue(int x) {
+        if (isFull()) {
+            return;
+        }
+        arr[size] = x;
+        size++;
+    }
+
+    // Dequeue
+    public void dequeue() {
+        if (isEmpty()) {
+            return;
+        }
+        for (int i = 1; i < size; i++) {
+            arr[i - 1] = arr[i];
+        }
+        size--;
+    }
+
+    // Get front element
+    public int getFront() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[0];
+    }
+    //Get last element
+    public int getRear() {
+    if (isEmpty()) {
+        return -1;
+    }
+    return arr[size - 1];
+    }
+
+
+}
+
+
