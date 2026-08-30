81e3e8e79bd7bc7cb77f8ab54b566a7fb6391d42
GarvGupta-2005 <garv.0603@gmail.com>
2026-08-30 13:19:32 +0530

    Refactor infixToPostfix and add isOperand method
    Refactored infixToPostfix method to improve clarity and maintainability. Added isOperand method to check for valid operands.

--- Final/src/Stack_Queue/Postfix_Prefix_Infix/infixToPostfix.java
+++ Final/src/Stack_Queue/Postfix_Prefix_Infix/infixToPostfix.java
@@ -1,53 +1,62 @@
 class Solution {
     
- 
-
-       private static int priority(char c) {
-           if (c == '^') {
-               return 3;
-           } else if (c == '*' || c == '/') {
-               return 2;
-           } else if (c == '+' || c == '-') {
-               return 1;
-           } else {
-               return 0;
-           }
-       }
-
-       public static String infixToPostfix(String s) {
-           StringBuilder ans = new StringBuilder();
-           Stack<Character> st = new Stack<>();
-
-           for (int i = 0; i < s.length(); i++) {
-               char cur = s.charAt(i);
-
-               // 1. Corrected operand condition
-               if (Character.isLetterOrDigit(cur)) {
-                   ans.append(cur);
-               } else if (cur == '(') {
-                   st.push(cur);
-               } else if (cur == ')') {
-                   while (!st.isEmpty() && st.peek() != '(') {
-                       ans.append(st.pop());
-                   }
-                   if (!st.isEmpty()) st.pop(); // Remove '('
-               } else { // Operator (+, -, *, /, ^)
-                   // 2 & 3. Handle precedence and associativity
-                   while (!st.isEmpty() && (
-                       (cur != '^' && priority(st.peek()) >= priority(cur)) ||
-                       (cur == '^' && priority(st.peek()) > priority(cur))
-                   )) {
-                       ans.append(st.pop());
-                   }
-                   st.push(cur);
-               }
-           }
-
-           while (!st.isEmpty()) {
-               ans.append(st.pop());
-           }
-
-           return ans.toString();
-       }
-   
+    private static boolean isOperand(char c){
+        if((c >= 'a' && c <= 'z') || (c>='A' && c<='Z') || (c>='0' && c<='9')){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    private static int priority(char c){
+        if( c == '^'){
+            return 3;
+        }else if (c == '*' || c == '/'){
+            return 2;
+        }else if (c == '+' || c == '-'){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    
+    public static String infixToPostfix(String s) {
+        // code here
+        StringBuilder ans = new StringBuilder("");
+        int i = 0; int n = s.length();
+        Stack<Character> st = new Stack<>();
+        
+        while(i < n){
+            char cur = s.charAt(i);
+            
+            if(isOperand(cur)){
+                ans.append(cur);
+            }else if(cur == '('){
+                st.push(cur);
+            }else if(cur == ')'){
+                while(!st.isEmpty() && st.peek()!='('){
+                    ans.append(st.pop());
+                }
+                st.pop();
+            }else{
+                //Operand so we will have to maintain associativity and precedence
+                while(!st.isEmpty() && ((cur == '^' && priority(st.peek()) > priority(cur))
+                || (cur != '^' && priority(st.peek()) >= priority(cur)))){
+                    ans.append(st.pop());
+                }
+                //We added the equality operatore so that if the operator is +,-,/,* we will have the left most opertor be executed first
+                // We did not add the equality here in order to maintain the right associativity of the power like a^b^c == a^(b^c)
+                st.push(cur);
+            }
+            
+            i++;
+        }
+        
+        while(!st.isEmpty()){
+            ans.append(st.pop());
+        }
+        
+        return ans.toString();
+        
+    }
 }
